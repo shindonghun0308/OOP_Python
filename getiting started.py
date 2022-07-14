4828e68ee9dcf92059c5a2199aa9bfaffe4b3b87
easthun <e0555808@u.nus.edu>
2022-07-15 02:11:33 +0800

    First Part Done
    42:00

--- getiting started.py
+++ getiting started.py
@@ -1,4 +1,5 @@
 class Item:
+    pay_rate = 0.8 #this is class attribute
     def __init__(self, name: str, price: float, quantity=0): #this occurs automatically during instantiation/ constructor
                                                                 # name only accepts str,
                                                                 #quantity: only accepts int cuz default value is int
@@ -7,7 +8,7 @@ class Item:
         assert price >= 0, f"Price {price} is not greater than zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
 
-        # Assign to self object
+        # Assign to self object/ instantiation attribute
         self.name = name # assigning an attribute to each instance that is created
         self.price = price #assigning an attribute like this doesnt mean that they cannot be added for specific instances
         self.quantity = quantity
@@ -19,7 +20,13 @@ class Item:
         return self.price * self.quantity #once the instances have been created, these attributes are made auto
     pass
 
-item1 = Item("Phone", 100, -1) #item1 is an instance of Item class
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate ## syntax for using class level attribute
+
+
+
+
+item1 = Item("Phone", 100, 3) #item1 is an instance of Item class
 # item1.name = "Phone" #addingt attributes
 # item1.price = 100
 # item1.quantity = 5
@@ -38,3 +45,16 @@ print(item1.calculate_total_price())
 
 # random_str = "aaa"
 # print(random_str.upper()) # this is how a pre-defined method/function in class is used, function inside class is called method
+
+print(Item.pay_rate) #class attribute
+print(item1.pay_rate) #it can access class level attribute
+
+print(Item.__dict__) # Show all the attributes for class level
+print(item1.__dict__) # All the contributes for instance level
+
+item1.pay_rate = 0.7 # this is in instance level
+Item.pay_rate # this is class level
+
+print(Item.pay_rate - item1.pay_rate ) #these are two diff attributes one in class level, one is instance level
+
+
